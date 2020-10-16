cdef985d976c1a8b221efe6763447c4f0290bca9
juzQrios <dj30c.1@gmail.com>
2020-10-16 18:28:53 +0530

    Refactor update logic

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -5,40 +5,54 @@ class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      now: new Date(),
-      endDate: new Date('10/25/2021 00:0 AM')
+      distance: props.to - new Date()
     };
-
-
   }
+
   componentDidMount() {
     this.timerID = setInterval(() => this.update(), 1000);
   }
+
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
+
   update() {
-    this.setState({
-      now: new Date()
-    });
+    this.setState((prevState) => ({
+      distance: prevState.distance - 1000
+    }));
   }
+
   render() {
     const _second = 1000,
       _minute = _second * 60,
       _hour = _minute * 60,
       _day = _hour * 24;
-    const distance = this.state.endDate - this.state.now;
-    const second = Math.floor(distance / _second),
-      minute = Math.floor(distance / _minute),
-      hour = Math.floor(distance / _hour),
-      day = Math.floor(distance / _day);
-    return (
-      <div id="countdown_timer" >
-        <div className="days">Days    : {day}</div>
-        <div className="hrs"><strong>Hours   : {hour}</strong></div>
-        <div className="mins">Minutes : {minute}</div>
-        <div className="secs">Seconds :{second}</div>
+
+    const {distance} = this.state;
+
+    const second_distance = Math.floor(distance / _second),
+      minute_distance = Math.floor(distance / _minute),
+      hour_distance = Math.floor(distance / _hour),
+      day_distance = Math.floor(distance / _day);
+
+    const timer = (
+      <div className="goal_timer">
+        <div className="days">Days : {day_distance}</div>
+        <div className="hrs"><strong>Hours : {hour_distance}</strong></div>
+        <div className="mins">Minutes : {minute_distance}</div>
+        <div className="secs">Seconds :{second_distance}</div>
       </div>
+    );
+
+    const timeUp = (
+      <div className="goal_timer">
+        Did you acheive your Goals?
+      </div>
+    )
+
+    return (
+      distance > 0 ? timer : timeUp
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import Timer from './Components/Timer/Timer.js';
 import './normalize.css';
 
-ReactDOM.render(<Timer />, document.getElementById('root'));
+ReactDOM.render(<Timer to={new Date('10/25/2021 00:0 AM')}/>, document.getElementById('root'));
