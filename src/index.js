da335c0446c5ce58d73a4489ec741be438229fe2
juzQrios <dj30c.1@gmail.com>
2020-10-16 17:44:02 +0530

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Timer from './Timer.js'
-import './normalize.css'
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import Timer from './Components/Timer/Timer.js';
+import './normalize.css';
 
 ReactDOM.render(<Timer />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
